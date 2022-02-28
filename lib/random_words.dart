3f04251c1c46ee8349d3e34703fce2db9d03cebd
Yony Home <30907894+yonyhome@users.noreply.github.com>
2022-02-27 22:34:21 -0500

    feat: firts commit

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -9,8 +9,11 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   String _theState = "0";
-  int _actualWordType = 0;
+  int tipo = 0;
+  int opcion = 0;
   final _random = new Random();
+  String texto = "";
+  int score = 0;
 
   int next(int min, int max) => min + _random.nextInt(max - min);
 
@@ -20,39 +23,59 @@ class _RandomWordsState extends State<RandomWords> {
     setRandomWord();
   }
 
-  void setRandomWord() {
+  String setRandomWord() {
     var option = next(0, 2);
     var randomItem = "";
     if (option == 0) {
       print("change to noun");
       randomItem = (nouns.toList()..shuffle()).first;
+      tipo = 1;
     } else {
       print("change to adjective");
       randomItem = (adjectives.toList()..shuffle()).first;
+      tipo = 2;
+    }
+    setState(() {
+      texto = randomItem;
+    });
+    return randomItem;
+  }
+
+  void _onPressed(int tipo, int opcion) {
+    if (tipo == opcion) {
+      setState(() {
+        score++;
+      });
+    } else {
+      setRandomWord();
     }
   }
 
-  void _onPressed() {}
-
-  void _onReset() {}
+  void _onReset() {
+    setState(() {
+      score = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Random Words"),
+        title: const Text("Nouns vs Adjectives"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Text("$_theState"),
+          Text(score.toString()),
+          Text(setRandomWord()),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ElevatedButton(
-                  onPressed: () => _onPressed(), child: Text("Noun")),
+                  onPressed: () => _onPressed(tipo, 2),
+                  child: const Text("Adjective")),
               ElevatedButton(
-                  onPressed: () => _onPressed(), child: Text("Adjective"))
+                  onPressed: () => _onPressed(tipo, 1), child: Text("Noun"))
             ],
           ),
           ElevatedButton(onPressed: () => _onReset(), child: Text("Reset")),
